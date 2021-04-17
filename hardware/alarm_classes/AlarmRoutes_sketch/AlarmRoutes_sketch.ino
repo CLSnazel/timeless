65f55d7094d43b86752025aacfb4b9557f38a208
Chantal Snazel <clsnazel@gmail.com>
2021-04-17 16:23:32 -0700

    feat: add rotue sketch folder, wifi credentials config file

---
+++ hardware/alarm_classes/AlarmRoutes_sketch/AlarmRoutes_sketch.ino
@@ -0,0 +1,40 @@
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+#include "Config.h"
+const char* ssid = wifi_name;
+const char* password =  wifi_password;
+ 
+AsyncWebServer server(80);
+ 
+void setup() {
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println(WiFi.localIP());
+ 
+  server.on(
+    "/post",
+    HTTP_POST,
+    [](AsyncWebServerRequest * request){},
+    NULL,
+    [](AsyncWebServerRequest * request, uint8_t *data, size_t len, size_t index, size_t total) {
+ 
+      for (size_t i = 0; i < len; i++) {
+        Serial.write(data[i]);
+      }
+ 
+      Serial.println();
+ 
+      request->send(204);
+  });
+ 
+  server.begin();
+}
+ 
+void loop() {}
