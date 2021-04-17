3c37c4ccf33e0156fe3eadb0659badbf1de92865
Meghan Hein <meghan.hein@gmail.com>
2021-04-16 19:13:56 -0700

    setup: CRA project scaffolding

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # timeless
+
+### server
+```sh
+$ cd server
+$ npm i
+$ cp .env.example .env
+$ npm run dev
+```
+
+### client
+``sh
+$ cd client
+$ yarn install && yarn start
+```
---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import 'theme/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        TIMELESS!
+      </header>
+    </div>
+  );
+}
+
+export default App;
